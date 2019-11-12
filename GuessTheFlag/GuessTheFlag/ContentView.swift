cd656b24ee550176a438879df47087c17e750223
Ma Xueyuan <machinemxy@me.com>
2019-11-13 00:15:58 +0900

    Day 34

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -27,6 +27,11 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     @State private var score = 0
     
+    // animation related
+    @State private var spinDegree = 0.0
+    @State private var opacity = 1.0
+    @State private var blur: CGFloat = 0
+    
     var body: some View {
         ZStack{
             LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
@@ -38,10 +43,22 @@ struct ContentView: View {
                 }
                 
                 ForEach(0 ..< 3) { number in
-                    Button(action: {
-                        self.flagTapped(number)
-                    }) {
-                        Flag(flagName: self.countries[number])
+                    if self.correctAnswer == number {
+                        Button(action: {
+                            self.flagTapped(number)
+                        }) {
+                            Flag(flagName: self.countries[number])
+                        }
+                        .rotation3DEffect(.degrees(self.spinDegree), axis: (x: 0, y: 1, z: 0))
+                        .blur(radius: self.blur)
+                    } else {
+                        Button(action: {
+                            self.flagTapped(number)
+                        }) {
+                            Flag(flagName: self.countries[number])
+                        }
+                        .opacity(self.opacity)
+                        .blur(radius: self.blur)
                     }
                 }
                 
@@ -62,15 +79,25 @@ struct ContentView: View {
         if number == correctAnswer {
             score += 1
             scoreTitle = "Correct"
+            withAnimation {
+                spinDegree += 360
+                opacity = 0.25
+            }
         } else {
             score -= 1
             scoreTitle = "Wrong! That's the flag of \(countries[number])"
+            withAnimation {
+                blur = 10
+            }
         }
         
         showingScore = true
     }
     
     func askQuestion() {
+        spinDegree = 0
+        opacity = 1.0
+        blur = 0
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
