30be9191169fe8d3415051d763ec6564c75dc594
Ma Xueyuan <machinemxy@me.com>
2019-11-17 21:41:18 +0900

    Day 37

---
+++ iExpense/iExpense/AddView.swift
@@ -0,0 +1,57 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Ma Xueyuan on 2019/11/17.
+//  Copyright © 2019 Ma Xueyuan. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var expenses: Expenses
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    @State private var showAlert = false
+    static let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add new expense")
+            .navigationBarItems(trailing:
+                Button("Save") {
+                    if let actualAmount = Int(self.amount) {
+                        let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
+                        self.expenses.items.append(item)
+                        self.presentationMode.wrappedValue.dismiss()
+                    } else {
+                        self.showAlert.toggle()
+                    }
+                }
+            )
+            .alert(isPresented: $showAlert) { () -> Alert in
+                Alert(title: Text("Error"), message: Text("Please input a correct number for amount"), dismissButton: .default(Text("OK")))
+            }
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
