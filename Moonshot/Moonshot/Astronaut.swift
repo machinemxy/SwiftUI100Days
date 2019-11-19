9912c23ee53b0b8ebf3fec276643d9a8e9f73f0f
Ma Xueyuan <machinemxy@me.com>
2019-11-19 21:34:56 +0900

    Day 40

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Ma Xueyuan on 2019/11/19.
+//  Copyright © 2019 Ma Xueyuan. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Ma Xueyuan on 2019/11/18.
+//  Copyright © 2019 Ma Xueyuan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    var body: some View {
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: Text("Detail View")) {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+                    
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                    }
+                }
+            }
+        .navigationBarTitle("Moonshot")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
