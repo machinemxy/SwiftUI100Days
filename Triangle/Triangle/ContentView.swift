b742eac8c065aaaba4cf9a5459dd580b0cdb5168
Ma Xueyuan <machinemxy@me.com>
2019-11-24 22:33:38 +0900

    Day 43

---
+++ Triangle/Triangle/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Triangle
+//
+//  Created by Ma Xueyuan on 2019/11/24.
+//  Copyright © 2019 Ma Xueyuan. All rights reserved.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Triangle()
+            .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            .frame(width: 300, height: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
