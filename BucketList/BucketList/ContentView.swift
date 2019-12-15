6c1056de54596e46990f8c7a65a736838d9762ad
Ma Xueyuan <machinemxy@me.com>
2019-12-15 22:50:38 +0900

    Day 69

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -7,26 +7,45 @@
 //
 
 import SwiftUI
-
-struct Cat: Codable {
-    var color: String
-}
+import LocalAuthentication
 
 struct ContentView: View {
+    @State private var isUnlocked = false
+    
     var body: some View {
-        Text("Hello, World!")
-        .onAppear(perform: saveCat)
-        .onTapGesture(perform: loadCat)
+        VStack {
+            if self.isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
     }
     
-    func saveCat() {
-        let cat = Cat(color: "white")
-        FileManager.default.save(cat, to: "cat.json")
-    }
-    
-    func loadCat() {
-        let cat: Cat = FileManager.default.load(from: "cat.json")
-        print(cat.color)
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                DispatchQueue.main.async {
+                    if success {
+                        // authenticated successfully
+                        self.isUnlocked = true
+                    } else {
+                        // there was a problem
+                    }
+                }
+            }
+        } else {
+            // no biometrics
+        }
     }
 }
 
